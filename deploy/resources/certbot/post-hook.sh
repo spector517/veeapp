104738c28de33cf417f3016e3a4c331d4263290c
spector517 <spector@borun.ru>
2026-04-26 10:36:30 +0300

    fixes 2

--- deploy/resources/certbot/post-hook.sh
+++ deploy/resources/certbot/post-hook.sh
@@ -5,4 +5,7 @@ if ufw status 2>/dev/null | grep -q "Status: active"; then
     ufw delete allow 80/tcp
 fi
 
-systemctl reload nginx
+if systemctl is-active --quiet nginx; then
+    systemctl reload nginx
+fi
+
